92675a9f7fc0b2457efab03cc591a584766d127b
vkolmakov <vkolmakov@hotmail.com>
2017-03-13 18:06:22 -0500

    Add boundaries for linkpoints

--- Homework2/setup.py
+++ Homework2/setup.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from matching import (latlon_to_xy, extract_shape_info_bounds)
 from utils import (in_chunks, add_items)
 
+
 db = SqliteDatabase('probe.db')
 
 class ProbeBase(Model):
@@ -36,7 +37,7 @@ class ProbePoint(ProbeBase):
 class LinkPoint(ProbeBase):
   linkPVID          = BigIntegerField()
   refNodeID         = BigIntegerField()
-  nrefNodeID        = BigIntegerField
+  nrefNodeID        = BigIntegerField()
   length            = DecimalField()
   functionalClass   = IntegerField()
   directionOfTravel = CharField()
@@ -50,7 +51,11 @@ class LinkPoint(ProbeBase):
   timeZone          = DecimalField()
   shapeInfo         = TextField() # array
   curvatureInfo     = TextField() # array
-  slopeInfo         = TextField() # array 
+  slopeInfo         = TextField() # array
+  minX              = DecimalField()
+  minY              = DecimalField()
+  maxX              = DecimalField()
+  maxY              = DecimalField()
 
   def get_csv_headers(): 
     return("linkPVID", 
@@ -103,18 +108,27 @@ def setup(db):
     insert_at_once = 500
     update_point = lambda point, x, y: add_items(point, [('x', x), ('y', y)])
     for raw_points in in_chunks(ProbeRead, insert_at_once):
-      points = [update_point(p, *latlon_to_xy((float(p['latitude']), float(p['longitude']))))
-                for p in raw_points]
-
+      points = (update_point(p, *latlon_to_xy((float(p['latitude']), float(p['longitude']))))
+                for p in raw_points)
       ProbePoint.insert_many(points).execute()
     
   # Link Data
   f = open('probe_data_map_matching/Partition6467LinkData.csv', 'rU')
   LinkRead = csv.DictReader(f, fieldnames=LinkPoint.get_csv_headers())
-    
-  with db.atomic(): 
-    for point in LinkRead:
-      LinkPoint.create(**point).save()
+
+  with db.atomic():
+    insert_at_once = 500
+    update_point = lambda point, mins, maxs: add_items(point,
+                                                       [('minX', mins[0]),
+                                                        ('minY', mins[1]),
+                                                        ('maxX', maxs[0]),
+                                                        ('maxY', maxs[1])])
+
+    for raw_points in in_chunks(LinkRead, insert_at_once):
+      points = (update_point(p, *extract_shape_info_bounds(p['shapeInfo']))
+                for p in raw_points)
+      LinkPoint.insert_many(points).execute()
+
   db_close_handler()
 
 def main(): 
