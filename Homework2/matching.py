af10ddadf3b5f13b76c4c74d639900376a25a342
vkolmakov <vkolmakov@hotmail.com>
2017-03-12 20:55:39 -0500

    Parameterize map dimensions and tolerance

--- Homework2/matching.py
+++ Homework2/matching.py
@@ -6,7 +6,8 @@ from shapely.geometry.polygon import Polygon
 from setup import (ProbePoint, LinkPoint, MatchedPoints)
 from utils import pairwise
 
-def lat_lon_to_x_y(latlon):
+
+def lat_lon_to_x_y(latlon, map_dimensions):
     '''
     Given a tuple with (lat, lon) produce a tuple with (x, y)
 
@@ -23,7 +24,6 @@ def lat_lon_to_x_y(latlon):
         lat_radians = lat * pi / 180
         return ln(tan((pi / 4) + (lat_radians / 2)));
 
-    (map_width, map_height) = (2000.0, 1000.0)
     (lat, lon) = latlon
 
     x = (lon + 180) * (map_width / 360)
@@ -31,22 +31,25 @@ def lat_lon_to_x_y(latlon):
 
     return (x, y)
 
-def shape_info_unit_to_point(shape_info_unit):
+def shape_info_unit_to_point(shape_info_unit, map_dimensions):
     '''
     Given a string with shape info unit return a shapely Point object.
 
     :param shape_info_unit: string with shape info in "lat/lon/elevation?"
     :type shape_info_unit: str
+    :param map_dimensions: a tuple with 2d map dimensions
+    :type map_dimensions: (float, float)
 
     :return: a shapely Point with x,y coordinates
     :rtype: shapely.geometry.Point
     '''
     [lat, lon, *_] = shape_info_unit.split('/')
-    (x, y) = lat_lon_to_x_y(float(lat), float(lon))
+    (map_width, map_height) = map_dimensions
+    (x, y) = lat_lon_to_x_y(float(lat), float(lon), map_dimensions)
 
     return Point(x, y)
 
-def is_near_line(line, point):
+def is_near_line(line, point, tolerance):
     '''
     Given a line and a point decide if point is near a line
 
@@ -54,11 +57,12 @@ def is_near_line(line, point):
     :type line: (shapely.geometry.Point, shapely.geometry.Point)
     :param point: a shapely point
     :type point: shapely.geometry.Point
+    :param tolerance: half of width of the polygon drawn around the line
+    :type tolerance: float
 
     :return: is the point near the line
     :rtype: bool
     '''
-    tolerance = 0.00001
     (p1, p2) = line
     ((x1, y1), (x2, y2)) = ((p1.x, p1.y), (p2.x, p2.y))
 
@@ -72,8 +76,22 @@ def is_near_line(line, point):
     return point.within(bounds)
 
 def belongs_to_link(link, point):
-    link_points = [shape_info_unit_to_point(p)
+    '''
+    Given a link and a point determine if point is close to a link
+
+    :param link: a road link object
+    :type link: setup.LinkPoint
+    :param point: a shapely point
+    :type point: shapely.geometry.Point
+
+    :return: does the point belong to a link
+    :rtype: bool
+    '''
+    tolerance = 0.00001
+    map_dimensions = (2000.0, 1000.0)
+
+    link_points = [shape_info_unit_to_point(p, map_dimensions)
                    for p in link.shapeInfo.split('|')]
 
-    return any(is_near_line(line, point)
+    return any(is_near_line(line, point, tolerance)
                for line in pairwise(link_points))
