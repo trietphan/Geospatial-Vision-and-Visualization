2ee0c98e3301401dd37093fc549c7412d11f46a1
vkolmakov <vkolmakov@hotmail.com>
2017-03-14 18:43:29 -0500

    Factor out getting matched probes

--- Homework2/main.py
+++ Homework2/main.py
@@ -2,33 +2,35 @@ from setup import (ProbePoint, LinkPoint)
 from matching import belongs_to
 
 
-def match_probes_to_links():
-    for link in LinkPoint.select().limit(200):
-        tolerance = 10
-        (min_x, min_y) = (link.minX, link.minY)
-        (max_x, max_y) = (link.maxX, link.maxY)
+def get_matched_probes(link):
+    tolerance = 10
+    (min_x, min_y) = (link.minX, link.minY)
+    (max_x, max_y) = (link.maxX, link.maxY)
 
-        # using raw query here because it's considerably faster
-        query_text = '''
-        SELECT * FROM probepoint
-        WHERE x > ? - {tolerance} AND
-              y > ? - {tolerance} AND
-              x < ? + {tolerance} AND
-              y < ? + {tolerance}
-        '''.format(tolerance=tolerance)
+    # using raw query here because it's considerably faster
+    query_text = '''
+    SELECT * FROM probepoint
+    WHERE x > ? - {tolerance} AND
+          y > ? - {tolerance} AND
+          x < ? + {tolerance} AND
+          y < ? + {tolerance}
+    '''.format(tolerance=tolerance)
 
-        candidate_points = ProbePoint.raw(query_text, min_x, min_y, max_x, max_y)
+    candidate_points = ProbePoint.raw(query_text, min_x, min_y, max_x, max_y)
 
-        belongs_to_link = belongs_to(link)
+    belongs_to_link = belongs_to(link)
 
-        matched_points = [p
-                          for p in candidate_points.execute()
-                          if belongs_to_link(p)]
+    matched_probe_points = [p
+                            for p in candidate_points.execute()
+                            if belongs_to_link(p)]
 
-        print('matched', len(matched_points), 'points')
+    return matched_probe_points
 
 def main():
-    match_probes_to_links()
+    for link in LinkPoint.select().limit(2):
+        print(link.shapeInfo)
+        matched_probes = get_matched_probes(link)
+        print('matched', len(matched_probes), 'probes')
 
 if __name__ == '__main__':
     main()
