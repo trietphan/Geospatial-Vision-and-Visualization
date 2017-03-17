48e2169b2a6ddf8f3b64268ae24c4959c4fb4f64
vkolmakov <vkolmakov@hotmail.com>
2017-03-16 20:36:49 -0500

    Update in_chunks to work with iterables of known length

--- Homework2/utils.py
+++ Homework2/utils.py
@@ -10,11 +10,20 @@ def pairwise(iterable):
 
 def in_chunks(iterable, size):
     '''s, 3 -> [s0,s1,s2], [s3,s4,s5], ...'''
-    while True:
-        chunk = list(islice(iterable, size))
-        if not chunk:
-            raise StopIteration
-        yield chunk
+
+    if '__len__' in dir(iterable):
+        iterable = list(iterable)
+        while iterable:
+            chunk = iterable[:size]
+            yield chunk
+            iterable = iterable[size:]
+        raise StopIteration
+    else:
+        while True:
+            chunk = list(islice(iterable, size))
+            if not chunk:
+                raise StopIteration
+            yield chunk
 
 def add_items(dictionary, items):
     result = dictionary.copy()
